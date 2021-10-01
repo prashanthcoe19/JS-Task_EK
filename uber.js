c7bc97c9c294aca20b858323224b7b285937bc88
Prashant Niroula <prashant.niroula67@gmail.com>
2021-10-01 14:56:44 +0545

    used forEach loop

--- uber.js
+++ uber.js
@@ -15,13 +15,13 @@ const uber = (arr) => {
   // if there are more than two zeroes the output array will all be zeros;
   if (count > 1) return arr2.fill(0);
 
-  for (let e of arr) {
+  arr.forEach((e) => {
     zero == true
       ? e === 0
         ? arr2.push(product)
         : arr2.push(0)
       : arr2.push(product / e);
-  }
+  });
   return arr2;
 };
 console.log(uber([1, 2, 3, 4, 0]));
